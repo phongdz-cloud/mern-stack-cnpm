24768645b569f548df0809146db8368da343b5b0
Phong Hồ <phonghh@unit.com.vn>
2022-10-09 22:11:46 +0700

    [PhongHH] finish folder 3-8

---
+++ backend/middlewares/user.js
@@ -0,0 +1,77 @@
+const { body } = require('express-validator');
+exports.registerValidation = () => {
+  return [
+    body('name')
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage('Please enter your name')
+      .isLength({ max: 30 })
+      .withMessage('Your name cannot exceed 30 characters'),
+    body('email')
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage('Please enter your email')
+      .isEmail()
+      .withMessage('Email invalid'),
+    body('password')
+      .trim()
+      .isLength({ min: 6 })
+      .withMessage('Your password must be longer than 6 characters'),
+  ];
+};
+
+exports.loginValidation = () => {
+  return [
+    body('email')
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage('Please enter your email')
+      .isEmail()
+      .withMessage('Email invalid'),
+    body('password')
+      .trim()
+      .isLength({ min: 6 })
+      .withMessage('Your password must be longer than 6 characters'),
+  ];
+};
+
+exports.forgotValidation = () => {
+  return [
+    body('email')
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage('Please enter your email')
+      .isEmail()
+      .withMessage('Email invalid'),
+  ];
+};
+
+exports.resetPasswordValidation = () => {
+  return [
+    body('password')
+      .trim()
+      .isLength({ min: 6 })
+      .withMessage('Your password must be longer than 6 characters'),
+    body('confirmPassword')
+      .trim()
+      .isLength({ min: 6 })
+      .withMessage('Your confirm password must be longer than 6 characters'),
+  ];
+};
+
+exports.updateUserProfileValidation = () => {
+  return [
+    body('email')
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage('Please enter your email')
+      .isEmail()
+      .withMessage('Email invalid'),
+    body('name')
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage('Please enter your name')
+      .isLength({ max: 30 })
+      .withMessage('Your name cannot exceed 30 characters'),
+  ];
+};
